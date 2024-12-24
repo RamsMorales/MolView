158b813d3a8ce3196864875fbc7cd464b8009dc3
Ramson Munoz Morales <103676659+RamsMorales@users.noreply.github.com>
2024-12-24 17:02:57 -0500

    Changed CMake to ignore GLUT with explicit pathing. Modified main to open window. Houston we have windows :P

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,39 @@
 #include <iostream>
+#include <GL/freeglut.h>
 
-int main() {
+// Default window dimensions
+int WIDTH = 1200, HEIGHT = 900;
 
-    std::cout << "Hello, Molview++!" << std::endl;
+// Display callback function
+void display() {
+    //glClearColor(0,0,1,0); //sets background color to blue
+    glClear(GL_COLOR_BUFFER_BIT); // Clears the screen
+    glFlush();                    // Renders the clear
+}
+
+// Keyboard callback function
+void handleKeyboard(unsigned char key, int x, int y){
+
+    std::cout << "You pressed the " << key << " key!" << std::endl;
+    
+    if (key == 27) {
+        glutLeaveMainLoop(); //close the window
+        
+    }
+}
+
+int main(int argc, char** argv) {
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+
+
+    glutInitWindowSize(WIDTH, HEIGHT);
+    glutCreateWindow("MolView++ Window Setup");
+
+    glutDisplayFunc(display);
+    glutKeyboardFunc(handleKeyboard);
+
+    glutMainLoop();
+
     return 0;
 }
